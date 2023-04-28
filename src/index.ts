e8afecd176adddd7c2ab508f364bac9d6698523c
Taechat Wangwaew <TaechatW@BETAGRO.com>
2023-04-28 09:42:20 +0700

    first commmit

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
